6a43f097e29dbb8f9ca772783361f1b1a7c5e016
Nsikak Eyo <mrnsikakeyo@gmail.com>
2026-01-25 14:16:42 +0100

    update

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,7 +2,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const yearSpan = document.getElementById("year");
     if (yearSpan) {
-        const currentYear = new Date().getFullYear();
-        yearSpan.textContent = currentYear;
+        yearSpan.textContent = new Date().getFullYear();
     }
 });

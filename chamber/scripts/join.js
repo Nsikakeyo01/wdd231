cae2f67d05a225040afb77cc0465b76ad7104282
Nsikak Eyo <mrnsikakeyo@gmail.com>
2026-01-25 14:02:47 +0100

    up

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,2 +1,8 @@
-document.getElementById("timestamp").value = new Date().toISOString();
-document.getElementById("year").textContent = new Date().getFullYear();
+// Set the current year in the footer
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+        const currentYear = new Date().getFullYear();
+        yearSpan.textContent = currentYear;
+    }
+});

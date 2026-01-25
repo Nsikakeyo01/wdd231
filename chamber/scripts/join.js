e8d4f3f7b69d979b45ca03d8f0bfdfcf4190eb24
Nsikak Eyo <mrnsikakeyo@gmail.com>
2026-01-25 07:55:46 +0100

    updat

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,3 @@
-document.getElementById('timestamp').value = new Date().toISOString();
+document.getElementById("timestamp").value = new Date().toISOString();
+
+document.getElementById("year").textContent = new Date().getFullYear();
